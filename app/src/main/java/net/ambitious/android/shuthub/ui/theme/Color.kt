3b3e83aa044af96f39525c7a5a7285ebbc96951b
bvlion <iwai.satoshi@zaim.co.jp>
2021-07-22 13:07:30 +0900

    色とかテーマ周り調整

--- app/src/main/java/net/ambitious/android/shuthub/ui/theme/Color.kt
+++ app/src/main/java/net/ambitious/android/shuthub/ui/theme/Color.kt
@@ -2,7 +2,9 @@ package net.ambitious.android.shuthub.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+object Color {
+  val Amber200 = Color(0xFFFFFF74)
+  val Amber500 = Color(0xFFFFD740)
+  val Amber700 = Color(0xFFC8A600)
+  val Teal200 = Color(0xFFFFCDD2)
+}
--- app/src/main/java/net/ambitious/android/shuthub/ui/theme/Theme.kt
+++ app/src/main/java/net/ambitious/android/shuthub/ui/theme/Theme.kt
@@ -2,20 +2,27 @@ package net.ambitious.android.shuthub.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Typography
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import net.ambitious.android.shuthub.ui.theme.Shape.Shapes
 
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
+object Theme {
+  private val DarkColorPalette = darkColors(
+    primary = Color.Amber200,
+    primaryVariant = Color.Amber700,
+    secondary = Color.Teal200
+  )
 
-private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+  private val LightColorPalette = lightColors(
+    primary = Color.Amber500,
+    primaryVariant = Color.Amber700,
+    secondary = Color.Teal200
 
     /* Other default colors to override
     background = Color.White,
@@ -25,20 +32,41 @@ private val LightColorPalette = lightColors(
     onBackground = Color.Black,
     onSurface = Color.Black,
     */
-)
+  )
 
-@Composable
-fun ShutHubTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
-  val colors = if (darkTheme) {
-    DarkColorPalette
-  } else {
-    LightColorPalette
-  }
+  private val Typography = Typography(
+    body1 = TextStyle(
+      fontFamily = FontFamily.Default,
+      fontWeight = FontWeight.Normal,
+      fontSize = 16.sp
+    )
+    /* Other default text styles to override
+    button = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.W500,
+        fontSize = 14.sp
+    ),
+    caption = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp
+    )
+    */
+  )
 
-  MaterialTheme(
+  @Composable
+  fun ShutHubTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+    val colors = if (darkTheme) {
+      DarkColorPalette
+    } else {
+      LightColorPalette
+    }
+
+    MaterialTheme(
       colors = colors,
       typography = Typography,
       shapes = Shapes,
       content = content
-  )
+    )
+  }
 }
